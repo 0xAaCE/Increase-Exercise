54b4924da49a49480277cb1860e247f8775e3e62
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-02 16:35:28 -0300

    Add routes

--- DB/app/routes.js
+++ DB/app/routes.js
@@ -1,5 +1,18 @@
+const { checkSchema } = require('express-validator');
+
 const { healthCheck } = require('./controllers/healthCheck');
+const { createPayment, getAllPayments } = require('./controllers/payments');
+const { createTransaction, getAllTransactions } = require('./controllers/transactions');
+const { validateSchemas } = require('./middlewares/schemas');
+const { paymentSchema } = require('./schemas/payments');
+const { transactionSchema } = require('./schemas/transactions');
 
 exports.init = app => {
   app.get('/health', healthCheck);
+
+  app.get('/transacionts', getAllPayments);
+  app.post('/transactions', [checkSchema(transactionSchema), validateSchemas], createTransaction);
+
+  app.get('/payments', getAllPayments);
+  app.post('/payments', [checkSchema(paymentSchema), validateSchemas], createPayment);
 };
