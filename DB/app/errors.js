c5cf5f3012a3659ffaf1633a32ee8689f00bb8a0
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-02 15:31:52 -0300

    Add some errors

---
+++ DB/app/errors.js
@@ -0,0 +1,22 @@
+const internalError = (message, internalCode) => ({
+  message,
+  internalCode
+});
+
+exports.DATABASE_ERROR = 'database_error';
+exports.databaseError = message => internalError(message, exports.DATABASE_ERROR);
+
+exports.DEFAULT_ERROR = 'default_error';
+exports.defaultError = message => internalError(message, exports.DEFAULT_ERROR);
+
+exports.CONECTION_ERROR = 'connection_error';
+exports.conectionError = message => internalError(message, exports.CONECTION_ERROR);
+
+exports.BAD_REQUEST = 'bad_request';
+exports.badRequest = message => internalError(message, exports.BAD_REQUEST);
+
+exports.FORBIDDEN_USER = 'forbidden_user';
+exports.forbiddenUser = message => internalError(message, exports.FORBIDDEN_USER);
+
+exports.NOT_FOUND = 'not_found';
+exports.notFound = message => internalError(message, exports.NOT_FOUND);
