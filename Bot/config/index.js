beda9197b2fb444d991a61f17c06e01b9598e6f0
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-04 15:24:46 -0300

    Add some constant to config

--- Bot/config/index.js
+++ Bot/config/index.js
@@ -15,6 +15,31 @@ exports.config = {
     databaseApi: {
       host: process.env.DB_HOST || 'localhost',
       port: process.env.DB_PORT || '8081'
+    },
+    id: {
+      pos: Number(process.env.ID_POS),
+      len: Number(process.env.ID_LEN),
+      clientPos: Number(process.env.ID_CLIENT_POS)
+    },
+    date: {
+      pos: Number(process.env.DATE_POS),
+      len: Number(process.env.DATE_LEN),
+      format: process.env.DATE_FROMAT
+    },
+    amount: {
+      len: Number(process.env.AMOUNT_LEN)
+    },
+    payment: {
+      currencyPos: Number(process.env.PAYMENT_CURRENCY_POS),
+      currencyLen: Number(process.env.PAYMENT_CURRENCY_LEN),
+      totalAmountPos: Number(process.env.PAYMENT_TOTALAMOUNT_POS),
+      totalDiscountPos: Number(process.env.PAYMENT_TOTALDISCOUNT_POS),
+      amountDiscountedPos: Number(process.env.PAYMENT_AMOUNTDISCOUNTED_POS)
+    },
+    transaction: {
+      amountPos: Number(process.env.TRANSACTION_AMOUNT_POS),
+      approvedPos: Number(process.env.TRANSACTION_APPROVED_POS),
+      approvedLen: Number(process.env.TRANSACTION_APPROVED_LEN)
     }
   }
 };
