db0ec9a528d1444f5c102302f8cb65dca8682133
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-04 23:32:28 -0300

    Add utils

---
+++ Bot/helpers/utils.js
@@ -0,0 +1,7 @@
+exports.asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+};
+
+exports.waitFor = ms => new Promise(r => setTimeout(r, ms));
