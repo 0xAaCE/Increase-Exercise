61b54018a16b2409f01fd85065ca87f55c990bec
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-04 23:32:59 -0300

    Change port of DB server

--- DB/server.js
+++ DB/server.js
@@ -2,7 +2,7 @@ const app = require('./app'),
   logger = require('./app/logger'),
   { config } = require('./config');
 
-const port = config.common.api.port || 8080;
+const port = config.common.api.port || 8081;
 
 Promise.resolve()
   .then(() => {
