ea9c2b59bdff8f6e5571f30b87ab089dc5be38b4
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-05 15:54:30 -0300

    Use sum from payments helper

--- Server/app/controllers/payments.js
+++ Server/app/controllers/payments.js
@@ -1,9 +1,10 @@
 const { getPayments } = require('../services/payments');
 const { info } = require('../logger');
+const { sum } = require('../helpers/payments');
 
 exports.getPayments = ({ params: { clientId } }, res, next) => {
   info(`Attempt to get payments with clientId ${clientId}`);
   return getPayments(clientId)
-    .then(payments => res.send(payments))
+    .then(payments => res.send(sum(payments)))
     .catch(next);
 };
