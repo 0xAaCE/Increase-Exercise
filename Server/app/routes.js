e4e77042d1988e9c3b3dd234fe6f09741728041f
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-05 09:27:41 -0300

    Add routes

--- Server/app/routes.js
+++ Server/app/routes.js
@@ -1,5 +1,12 @@
 const { healthCheck } = require('./controllers/healthCheck');
+const { getPayments } = require('./controllers/payments');
+const { getTransactions } = require('./controllers/transactions');
+const { getClient } = require('./controllers/clients');
 
 exports.init = app => {
   app.get('/health', healthCheck);
+
+  app.get('/clients/:clientId/transactions', getTransactions);
+  app.get('/clients/:clientId/payments', getPayments);
+  app.get('/clients/:id', getClient);
 };
