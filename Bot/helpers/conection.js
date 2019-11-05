d59e804afd0934c542a2f8a1fdc8c0caf242405e
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-04 23:32:06 -0300

    Implement send transactions and send paymenv

--- Bot/helpers/conection.js
+++ Bot/helpers/conection.js
@@ -1,13 +1,32 @@
-const { get } = require('axios');
+const { get, post } = require('axios');
 
-const { increaseApi: increase } = require('../config').config.common;
+const { increaseApi: increase, databaseApi: db } = require('../config').config.common;
 const { logger } = require('../logger');
+const { asyncForEach, waitFor } = require('./utils');
 
 exports.obtainRawData = () => {
+  logger.info('Attempt to obtain raw data');
   return get(`${increase.url}/file.txt`, { headers: { Authorization: increase.token } })
     .then(response => response.data)
     .catch(error => {
       logger.error(error.message);
       throw error;
     });
+};
+
+exports.sendTransactions = transactions => {
+  logger.info('Attempt to send transactions');
+  return asyncForEach(transactions, async transaction => {
+    await waitFor(50);
+    return post(`${db.url}/transactions`, transaction).catch(error => {
+      logger.error(error.message);
+    });
+  });
+};
+
+exports.sendPayment = payment => {
+  logger.info('Attempt to send payment');
+  return post(`${db.url}/payments`, payment).catch(error => {
+    logger.error(error.message);
+  });
 };
