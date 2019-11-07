62921348e608a26bfdb0a2be771b6abf874cd000
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-07 08:40:09 -0300

    HotFix

--- DB/app/services/payments.js
+++ DB/app/services/payments.js
@@ -3,12 +3,12 @@ const { databaseError } = require('../errors');
 
 exports.createPayment = newPayment => {
   return Payment.create(newPayment).catch(error => {
-    throw databaseError(error);
+    throw databaseError(error.message);
   });
 };
 
 exports.getAllPaymentsBy = where => {
   return Payment.findAll({ where }).catch(error => {
-    throw databaseError(erro);
+    throw databaseError(error.message);
   });
 };
