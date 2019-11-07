4cd611ca5304f2693a86ca08f559b6b7df27661b
alejoacevedo <ale.acevedo@live.com.ar>
2019-11-07 08:40:18 -0300

    HotFix

--- DB/app/services/transactions.js
+++ DB/app/services/transactions.js
@@ -3,12 +3,12 @@ const { databaseError } = require('../errors');
 
 exports.createTransaction = newTransaction => {
   return Transaction.create(newTransaction).catch(error => {
-    throw databaseError(error);
+    throw databaseError(error.message);
   });
 };
 
 exports.getAllTransactionsBy = where => {
   return Transaction.findAll({ where }).catch(error => {
-    throw databaseError(erro);
+    throw databaseError(error.message);
   });
 };
